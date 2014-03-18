6ab779cf97b36db886ddd1d122aca772758e8ba7
evandrosaraiva <saraiva.evandro@gmail.com>
2014-03-18 15:28:51 -0300

    inclusão da opção centro de custo.

--- application/relatorio/view/listar.php
+++ application/relatorio/view/listar.php
@@ -3,6 +3,7 @@
 ?>
 <div class="w-box">
     <div class="w-box-header"><h4>Relatório</h4></div>
+
     <div class="w-box-content cnt_a">
         <form enctype="multipart/form-data" method="post" action="<?=$this->url('relatorio/dados')?>" name="form" id="validate">
 
@@ -12,25 +13,24 @@
                         <div class="controls">
                             <select name="tipoRelatorio" id="tipoRelatorio">
                                 <option value="geral">Geral</option>
-
 <!--                                <option value="periodo">Periodo</option>-->
                                 <option value="conciliacao">Conciliação Bancária</option>
-                                <option value="contas_pagar">Contas a Pagar</option>
-                                <option value="contas_receber">Contas a Receber</option>
+                                <option value="centroCusto">Centro de Custo</option>
                             </select>
                         </div>
                         <div class="clear"></div>
                     </div>
                 </div>
+
             <fieldset class="geral">
                 <div class="formSep">
                     <div class="control-group">
                         <div class="control-label"><label>Mês: <span class="f_req">*</span></label></div>
                         <div class="controls">
                             <select name="mes" class="validate[required]">
-                                <?php foreach($meses as $key => $mes){ ?>
-                                    <option value="<?=$key?>"><?=$mes?></option>
-                                <?php }; ?>
+                                 <?php foreach($meses as $key => $mes){ ?>
+                                     <option value="<?=$key?>"><?=$mes?></option>
+                                 <?php }; ?>
                             </select>
                         </div>
                         <div class="clear"></div>
@@ -47,102 +47,6 @@
                     </div>
                 </div>
             </fieldset>
-            <fieldset class="contas_pagar" style="display: none">
-                <div class="formSep">
-                    <div class="control-group">
-                        <div class="control-label"><label>Data Inicial: <span class="f_req">*</span></label></div>
-                        <div class="controls">
-                            <input type="text" name="contas_pagar[dataInicial]" class="validate[required] maskData"/>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-
-                <div class="formSep">
-                    <div class="control-group">
-                        <div class="control-label"><label>Data Final: <span class="f_req">*</span></label></div>
-                        <div class="controls">
-                            <input type="text" name="contas_pagar[dataFinal]" class="validate[required] maskData"/>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-
-                <div class="formSep">
-                    <div class="control-group">
-                        <div class="control-label"><label>Fornecedor: <span class="f_req">*</span></label></div>
-                        <div class="controls">
-                            <select name="contas_pagar[fornecedor]" class="validate[required]">
-                                <option value="">Todos</option>
-                                <?php foreach($colecao->fornecedor as $item){ ?>
-                                    <option value="<?=$item->id?>"><?=$item->nome?></option>
-                                <?php }; ?>
-                            </select>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-                <div class="formSep">
-                    <div class="control-group">
-                        <div class="control-label"><label>Baixados: <span class="f_req">*</span></label></div>
-                        <div class="controls">
-                            <input type="radio" name="contas_pagar[baixados]" class="" checked value="1"/> Sim <br />
-                            <input type="radio" name="contas_pagar[baixados]" class="" value="2"/> Não <br />
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-            </fieldset>
-            <fieldset class="contas_receber" style="display: none">
-                <div class="formSep">
-                    <div class="control-group">
-                        <div class="control-label"><label>Data Inicial: <span class="f_req">*</span></label></div>
-                        <div class="controls">
-                            <input type="text" name="contas_receber[dataInicial]" class="validate[required] maskData"/>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-
-                <div class="formSep">
-                    <div class="control-group">
-                        <div class="control-label"><label>Data Final: <span class="f_req">*</span></label></div>
-                        <div class="controls">
-                            <input type="text" name="contas_receber[dataFinal]" class="validate[required] maskData"/>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-                <div class="formSep">
-                    <div class="control-group">
-                        <div class="control-label"><label>Cliente: <span class="f_req">*</span></label></div>
-                        <div class="controls">
-                            <select name="contas_receber[cliente]" class="validate[required]">
-                                <option value="">Todos</option>
-                                <?php foreach($colecao->fornecedor as $item){ ?>
-                                    <option value="<?=$item->id?>"><?=$item->nome?></option>
-                                <?php }; ?>
-                            </select>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-                <div class="formSep">
-                    <div class="control-group">
-                        <div class="control-label"><label>Tipo de Comissão: <span class="f_req">*</span></label></div>
-                        <div class="controls">
-                            <select name="contas_receber[tipo_comissao]" class="validate[required]">
-                                <option value="">Todos</option>
-                                <?php foreach($colecao->tipoComissao as $item){ ?>
-                                    <option value="<?=$item->id?>"><?=$item->nome?></option>
-                                <?php }; ?>
-                            </select>
-                        </div>
-                        <div class="clear"></div>
-                    </div>
-                </div>
-            </fieldset>
-
             <fieldset class="periodo" style="display: none">
                 <div class="formSep">
                     <div class="control-group">
@@ -164,6 +68,22 @@
                     </div>
                 </div>
             </fieldset>
+            <fieldset class="centroCusto" style="display: none">
+                <div class="formSep">
+                    <div class="control-group">
+                        <div class="control-label"><label>Centro de Custo: <span class="f_req">*</span></label></div>
+                        <div class="controls">
+                            <select name="centroCusto">
+                                <?php foreach($colecao->centrosCusto as $item){ ?>
+                                    <option value="<?=$item->id?>"><?=$item->nome?></option>
+                                <?php }; ?>
+                            </select>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+            </fieldset>
+
             <fieldset class="conciliacao" style="display: none">
                 <div class="formSep">
                     <div class="control-group">
